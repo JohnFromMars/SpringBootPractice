dbe106f36bc17e9331be38ed76fa205ad03d7f2d
user <user@user-PC.lan>
2017-04-18 15:36:15 +0800

    2017/04/18

---
+++ spring-boot/src/main/java/com/marsbase/springboot/model/StatusUpdateDao.java
@@ -0,0 +1,7 @@
+package com.marsbase.springboot.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StatusUpdateDao extends CrudRepository<StatusUpdate, Long> {
+
+}
--- spring-boot/src/test/java/com/marsbase/springboot/test/StatusTest.java
+++ spring-boot/src/test/java/com/marsbase/springboot/test/StatusTest.java
@@ -1,25 +1,43 @@
 package com.marsbase.springboot.test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.marsbase.springboot.App;
+import com.marsbase.springboot.model.StatusUpdate;
+import com.marsbase.springboot.model.StatusUpdateDao;
 
-
+@Transactional
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(App.class)
 public class StatusTest {
 
+	@Autowired
+	private StatusUpdateDao statusUpdateDao;
+
 	@Test
-	public void testDummy() {
-		Long val = 7L;
+	public void testSave() {
+
+		StatusUpdate statusUpdate = new StatusUpdate("this test status");
+
+		statusUpdateDao.save(statusUpdate);
+
+		assertNotNull("id is null", statusUpdate.getId());
+		assertNotNull("added date is null", statusUpdate.getAdded());
+
+		StatusUpdate retrieved = statusUpdateDao.findOne(statusUpdate.getId());
 		
-		assertNotNull(val);
-		
+		assertEquals("None-Matching statusUpdate",statusUpdate, retrieved);
+
 	}
 }
