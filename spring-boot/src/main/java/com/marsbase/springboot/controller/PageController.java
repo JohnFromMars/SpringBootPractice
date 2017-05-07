f93695e49f6b26610bc994d38c808386b7d2f00e
user <user@10.233.65.181>
2017-05-07 18:42:21 +0800

    2017/05/07

--- spring-boot/src/main/java/com/marsbase/springboot/controller/PageController.java
+++ spring-boot/src/main/java/com/marsbase/springboot/controller/PageController.java
@@ -1,15 +1,34 @@
 package com.marsbase.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.marsbase.springboot.model.StatusUpdate;
+import com.marsbase.springboot.service.StatusUpdateService;
 
 @Controller
 public class PageController {
 
+	@Autowired
+	private StatusUpdateService statusUpdateService;
+
+	/**
+	 * Home page, show the latest update
+	 * 
+	 * @param modelAndView
+	 * @return
+	 */
 	@RequestMapping("/")
-	public String home() {
-		return "app.homepage";
+	public ModelAndView home(ModelAndView modelAndView) {
+
+		StatusUpdate statusUpdate = statusUpdateService.getLatest();
+		
+		modelAndView.setViewName("app.homepage");
+		modelAndView.getModel().put("statusUpdate", statusUpdate);
+
+		return modelAndView;
 	}
 
 	@RequestMapping("/about")
